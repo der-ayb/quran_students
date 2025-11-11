5e82d7afd5603cc09d96134f98ac670899c57b8a
der-ayb <derrafa6@email.com>
2025-11-11 08:43:38 +0000

    modified:   app.html 	modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ workbox.precaching.precacheAndRoute(
     { url: "./", revision: "1" },
     { url: "./favicon.ico", revision: "1" },
     { url: "./index.html", revision: "1" },
-    { url: "./app.html", revision: "1" },
+    { url: "./app.html", revision: "2" },
     { url: "./manifest.json", revision: "1" },
 
     { url: "./src/style.css", revision: "1" },
